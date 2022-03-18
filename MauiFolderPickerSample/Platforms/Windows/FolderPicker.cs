68771066b00e1cfa7cd7dbeb41a7fa402b54476a
Gerald Versluis <gerald@verslu.is>
2022-03-18 19:46:14 +0100

    Update FolderPicker.cs

--- MauiFolderPickerSample/Platforms/Windows/FolderPicker.cs
+++ MauiFolderPickerSample/Platforms/Windows/FolderPicker.cs
@@ -9,7 +9,7 @@ namespace MauiFolderPickerSample.Platforms.Windows
             var folderPicker = new WindowsFolderPicker();
 
             // Get the current window's HWND by passing in the Window object
-            var hwnd = ((MauiWinUIWindow)App.Current.Windows[0].Handler.NativeView).WindowHandle;
+            var hwnd = ((MauiWinUIWindow)App.Current.Windows[0].Handler.PlatformView).WindowHandle;
 
             // Associate the HWND with the file picker
             WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
