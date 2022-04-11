aac278df57491609812a38537f76f8636f77fec4
Gerald Versluis <gerald@verslu.is>
2022-04-11 12:58:29 +0200

    Updated for latest .NET MAUI release

--- MauiFolderPickerSample/Platforms/Windows/FolderPicker.cs
+++ MauiFolderPickerSample/Platforms/Windows/FolderPicker.cs
@@ -6,7 +6,9 @@ namespace MauiFolderPickerSample.Platforms.Windows
     {
         public async Task<string> PickFolder()
         {
-            var folderPicker = new WindowsFolderPicker();
+            var folderPicker = new WindowsFolderPicker();    
+            // Might be needed to make it work on Windows 10
+            folderPicker.FileTypeFilter.Add("*");
 
             // Get the current window's HWND by passing in the Window object
             var hwnd = ((MauiWinUIWindow)App.Current.Windows[0].Handler.PlatformView).WindowHandle;
